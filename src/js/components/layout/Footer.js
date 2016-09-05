549a062077662aa106b188b13a6212015d793c22
Rahul Gupta <rahulg.active@gmail.com>
2016-09-05 22:58:20 +0530

    Node2

--- src/js/components/layout/Footer.js
+++ src/js/components/layout/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
       <footer>
         <div class="row">
           <div class="col-lg-12">
-            <p>dhoom rahulg</p>
+            <p>dhoom rahulg123 ghpu</p>
           </div>
         </div>
       </footer>
