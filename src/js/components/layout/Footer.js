bfb6dc2babad2d43a12224b43f002249ca4b99de
Gupta <rgupta5@ceb.com>
2016-09-05 22:41:53 +0530

    Dhoom 3123

--- src/js/components/layout/Footer.js
+++ src/js/components/layout/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
       <footer>
         <div class="row">
           <div class="col-lg-12">
-            <p>dhoom 3</p>
+            <p>dhoom 3123</p>
           </div>
         </div>
       </footer>
